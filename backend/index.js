94af09c4bf088cfd2193d52467220f90c92c6f28
Chris Pham <chris@Chriss-MacBook-Pro-2.local>
2021-11-21 13:52:22 -0800

    user Schema

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose
-    .connect(process.env.MONGO_URL, { useNewUrlParser: true})
+    .connect(process.env.MONGO_URL, { 
+        useNewUrlParser: true,
+        useUnifiedTopology : true
+    })
     .then(() => {
         console.log("MongoDB CONNECTED");
     })
     .catch((err) => console.log(err));
+
+    
 
 app.listen(8000, ()=> {
     console.log("Backend server is running")
