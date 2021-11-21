b975dddc058532a81a8a568f995baca4330b0f2e
Chris Pham <chris@Chriss-MacBook-Pro-2.local>
2021-11-21 13:46:24 -0800

    adding mongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
+
 app.listen(8000, ()=> {
     console.log("Backend server is running")
 })
