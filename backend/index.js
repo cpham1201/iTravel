eeb0387001665a2fd6d5b70d8a56ee6247d92495
Chris Pham <chris@Chriss-MacBook-Pro-2.local>
2021-11-21 13:49:07 -0800

    updating MongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose 
- .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })   
- .then(() => console.log("MongoDB connected!"))
- .catch(err => console.log(err));
+mongoose
+    .connect(process.env.MONGO_URL, { useNewUrlParser: true})
+    .then(() => {
+        console.log("MongoDB CONNECTED");
+    })
+    .catch((err) => console.log(err));
 
 app.listen(8000, ()=> {
     console.log("Backend server is running")
